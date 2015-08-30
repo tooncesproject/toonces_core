39ec3f43fee932cacd6ebce1ae73f06988bca75d
Paul Anderson <elpablogrande@gmail.com>
2015-08-30 14:20:13 -0700

    recursion fixed yay

--- index.php
+++ index.php
@@ -34,31 +34,25 @@ function getPage($pathString, $conn) {
 		// recursively query pages tables until end is reached
 		$pageSearchResult = pageSearch($pathArray, $defaultPage, $depthCount, $conn);
 		
-		echo 'getPage returning '.$pageSearchResult.'<br>';
 		return $pageSearchResult;
 	}
 }
 
 function pageSearch($pathArray, $pageid, $depthCount, $conn) {
 	
-	echo implode('/',$pathArray).PHP_EOL.'<BR>';
-	echo 'depth count: '.$depthCount.PHP_EOL.'<br>';
 	$pageFound = false;
 	$descendantPageId;
 		
 	$query = sprintf(file_get_contents(ROOTPATH.'/sql/retrieve_child_page_ids.sql'),$pageid);
-	echo $query.'<br>';
+	
 	
 	$descenantPages = $conn->query($query);
 	
 	if (!$descenantPages) {
-		echo 'descendant not found, returning '.$pageid;
 		return $pageid;
 	}
 	
 	foreach ($descenantPages as $row) {
-			echo 'sql pathname: '.$row['pathname'].'<br>'.PHP_EOL;
-			echo 'path array name for '.$depthCount.': '.$pathArray[$depthCount].'<br>'.PHP_EOL;
 		
 		if ($row['pathname'] == $pathArray[$depthCount]) {
 			$descendantPageId = $row['descendant_page_id'];
@@ -69,25 +63,14 @@ function pageSearch($pathArray, $pageid, $depthCount, $conn) {
 	// if a page was found and the end of the array has been reached, return the descendant ID
 	// otherwise continue recursion
 	$nextDepthCount = ++$depthCount;
-	echo 'next depth count :'.$nextDepthCount.'<br>';
 	
-	if (!array_key_exists($nextDepthCount, $pathArray))
-		echo 'next array key not found<br>';
-	else
-		echo 'next key array exists<br>';
-	if ($pageFound)
-		echo 'the page was found <br>';
-	else 
-		echo 'the page was not found <br> ';
-				
-	if ($pageFound && !array_key_exists($nextDepthCount, $pathArray)) {
-		echo 'returning descendant:'.$descendantPageId.'<br>';
+	
+	if ($pageFound && (!array_key_exists($nextDepthCount, $pathArray) OR trim($pathArray[$nextDepthCount]) == '')) {
 		return $descendantPageId;
 		
-	} else if ($pageFound && array_key_exists($nextDepthCount, $pathArray)) {
-		echo 'page found; iterating recursion. <br>';
-		pageSearch($pathArray, $descendantPageId, $nextDepthCount, $conn);
-		return 0;
+	} else if ($pageFound) {
+		//iterate recursion if page found
+		return pageSearch($pathArray, $descendantPageId, $nextDepthCount, $conn);
 		
 	} else {
 	
@@ -100,14 +83,13 @@ function pageSearch($pathArray, $pageid, $depthCount, $conn) {
 		}
 	
 		if ($redirectOnError) {
-			echo 'returning '.$pageid;
 			return $pageid;
 		}
 		else {
-			echo 'returning 0<br>';
 			return 0;
 		}
 	}
+	
 }
 
 
@@ -132,13 +114,7 @@ $path = substr($path,1,strlen($path)-1);
 $conn = UniversalConnect::doConnect();
 
 // Check for a URL query string. If none, defaults to home page.
-/*
-if (trim($path)) {
-	$query = sprintf(file_get_contents(ROOTPATH.'/sql/retrieve_page.sql'),$path,1);
-} else {
-	$query = file_get_contents(ROOTPATH.'/sql/retrieve_home_page.sql');
-}
-*/
+
 if (trim($path))
 	$pageId = getPage($path, $conn);	
 
