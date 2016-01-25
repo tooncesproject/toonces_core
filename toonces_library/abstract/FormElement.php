b8ad121362e710a948117ac5ac7e9dc071ef7541
Paul Anderson <elpablogrande@gmail.com>
2016-01-25 09:57:37 -0800

    begin abstraction refactor

--- toonces_library/abstract/FormElement.php
+++ toonces_library/abstract/FormElement.php
@@ -1,4 +1,11 @@
 <?php
+/*
+	The FormElement renders HTML and handles response for html forms and POSTdata.
+	Its responsibilities are:
+		Iterate through an array of FormElementInput objects to render HTML
+		Decide what to do with the input
+
+*/
 
 require_once ROOTPATH.'/toonces.php';
 
@@ -9,12 +16,50 @@ abstract class FormElement extends Element
 	// var $htmlHeader;
 	// var $htmlFooter;
 	// var $pageViewReference;
+	
+	// Abstract variables:
+	
+	// Binary success/fail state (doesn't have to be binary)
 	private $success;
 	
+	// Array of FormElementInput objects
+	private $formArray = array();
+	
+	// Settings
+	private $responseType;
+	private $htmlTag;
+	private $cssClass;
+	
+	// Default settings
+	private $responseTypeDefault;
+	private $htmlTagDefault;
+	private $cssClassDefault;
+	
 	public function __construct($pageView) {
+		// __construct handles the basic responsibilities of the element:
+			// interface compliance
+			// Element subclass responsibilities
+			// set default setting values
+		
 		$this->pageViewReference = $pageView;
-		$this->htmlHeader = '<div class="form_element>';
-		$this->htmlFooter = '</div>';
+		//	$this->htmlHeader = '<div class="form_element>';
+		//	$this->htmlFooter = '</div>';
+	}
+
+	function buildFormArray() {
+		// This function holds customizations for building the form array.
+		// Its responsibility is to add members to the formArray[] instance variable.
+		// FormElementInput objects will be rendered in the order they are added here.
+		// Also, this function handles 
+	}
+	
+	function responseStateHandler($responseState) {
+		
+	}
+	
+	function objectSettings() {
+		// Customizable method to override default settings if desired.
+		
 	}
 	
 }
---
+++ toonces_library/abstract/FormElementInput.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * FormElementInput
+ * Initial commit: Paul Anderson 1/25/16
+ * 
+ * This class holds inputs to be handled by the FormElement class
+ * The responsibilities of the inputs are:
+ * 		Generate html for the input within the form
+ * 		Gather the postdata from the input
+ * 		Set the type of the input
+ * 		Display input-level responses
+ * 
+ */
+
