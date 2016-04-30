243e4ad8346d90522f415aa8784ec156b8cd4346
Paul Anderson <elpablogrande@gmail.com>
2016-04-30 15:57:10 -0700

    added cancel post option

--- toonces_library/userInterface/BlogToolbarElement.php
+++ toonces_library/userInterface/BlogToolbarElement.php
@@ -3,6 +3,7 @@
  * BlogToolbarElement
  *
  * Initial Commit: Paul Anderson 2/7/2015
+ * Refactored as derivative from BlogPostToolbarElement 4/29/2016.
  *
  */
 
@@ -15,48 +16,84 @@ class BlogToolbarElement extends ToolbarElement
 	// user state
 	var $userCanEdit;
 
+	// utility stuff
+	// Array of this page's parsed URI
+	var $urlArray = array();
+	// Array of this page's parsed url parameters
+	var $currentParams = array();
+	var $currentParamsMode;
+
 	public function buildToolElement() {
 
 		// Tool's capabilities:
 		// 	publish page
 		//	unpublish page
-		//  Create new blog post
 
-		$this->newPostElement = new Element($this->pageViewReference);
+		// Set URL variables common to all tools
+		$thisPageURI = $_SERVER['REQUEST_URI'];
+		$this->urlArray = parse_url($thisPageURI);
+
+		if (isset($this->urlArray['query'])) {
+			parse_str($this->urlArray['query'], $this->currentParams);
+		}
+
+		$this->currentParamsMode = isset($this->currentParams['mode']) ? $this->currentParams['mode'] : '';
+
+		// Build the new post tool
+		$newPostToolElement = new Element($this->pageViewReference);
+		$newPostToolElement->setHTML($this->buildNewpostToolElementHTML());
+		$this->toolElement->addElement($newPostToolElement);
 
 
-		$newPostLinkHTML = '';
 
-		$urlArray = parse_url($_SERVER['REQUEST_URI']);
-		// Build URL for new post link
-		$newpostUrlArray = $urlArray;
-		$urlPath = $newpostUrlArray['path'];
-		if (isset($urlArray['query']))
-			parse_str($urlArray['query'], $params);
+	}
+
+	function buildNewpostToolElementHTML() {
+
+		$editPostLinkHTML = '';
+
+		if (isset($this->urlArray['query'])) {
+			parse_str($this->urlArray['query'], $linkParams);
+		}
+
+		// Build URL for edit post link
+		$editPostUrlArray = $this->urlArray;
+		$urlPath = $editPostUrlArray['path'];
 
 		// Add parameter
-		$params['mode'] = 'newpost';
-		$newpostUrlArray['query'] = http_build_query($params);
-		$newPostURL = $newpostUrlArray['path'].'?'.$newpostUrlArray['query'];
+		$linkParams['mode'] = 'newpost';
+		$editPostUrlArray['query'] = http_build_query($linkParams);
+		$editPostURL = $editPostUrlArray['path'].'?'.$editPostUrlArray['query'];
 
-
-		$htmlOut = <<<HTML
+		$editPageToolHTML= <<<HTML
 		<div class="TE_pagetoolelement">
-			<p>Hey, that's a good lookin' blog you got there!</p>
+			<p>Hey, that's a good lookin' blog you got there!.</p>
 HTML;
 
-		$htmlOut = $htmlOut.PHP_EOL;
+		$editPageToolHTML = $editPageToolHTML.PHP_EOL;
 
-		// If the user has editing privileges, add a link create new post
-		if ($this->userCanEdit == true)
-			$newPostLinkHTML = '<p><a href="'.$newPostURL.'"><telink>Create a New Blog Post</telink></a></p>'.PHP_EOL;
+		// If the user has editing privileges, add a blog editing link
+		if ($this->userCanEdit == true) {
+			// If we're in newpost mode, make it a 'cancel' link, otherwise an 'new post' link
 
-		$htmlOut = $htmlOut.$newPostLinkHTML;
+			if ($this->currentParamsMode == 'newpost') {
+				unset($linkParams['mode']);
+				$editPostUrlArray['query'] = http_build_query($linkParams);
+				if (empty($editPostUrlArray['query'])) {
+					$cancelPostURL = $editPostUrlArray['path'];
+				} else {
+					$cancelPostURL = $editPostUrlArray['path'].'?'.$editPostUrlArray['query'];
+				}
+				$editPostLinkHTML = '<p><a href="'.$cancelPostURL.'"><telink>Discard changes and cancel</telink></a></p>'.PHP_EOL;
+			} else {
+				$editPostLinkHTML = '<p><a href="'.$editPostURL.'"><telink>Create a new blog post</telink></a></p>'.PHP_EOL;
+			}
+		}
+		$editPageToolHTML = $editPageToolHTML.$editPostLinkHTML;
 
-		$htmlOut = $htmlOut.'</div>'.PHP_EOL;
+		$editPageToolHTML = $editPageToolHTML.'</div>'.PHP_EOL;
 
-		$this->newPostElement->setHTML($htmlOut);
-		$this->toolElement->addElement($this->newPostElement);
+		return $editPageToolHTML;
 	}
 
 }
