b554dc8ef2e76e843d71917843d1dab6be250a36
Paul Anderson <elpablogrande@gmail.com>
2018-03-01 16:56:42 -0800

    auth bug

--- toonces_library/php/utility/CoreAPIPageBuilderDelegate.php
+++ toonces_library/php/utility/CoreAPIPageBuilderDelegate.php
@@ -20,15 +20,17 @@ class CoreAPIPageBuilderDelegate extends APIPageBuilderDelegate implements iAPIP
     function authenticateUser() {
         // Toonces Core Services API uses Basic Auth for authentication, and the same 
         // user structure as Toonces Admin.
-        $email = $_SERVER['PHP_AUTH_USER'];
-        $pw = $_SERVER['PHP_AUTH_PW'];
-        $sessionManager = $this->pageView->sessionManager;
-        
         $userID = NULL;
-        $loginSuccess = $sessionManager->login($email, $pw);
-        if ($loginSuccess)
-            $userID = $sessionManager->userId;
-        
+        if (array_key_exists('PHP_AUTH_USER', $_SERVER) && array_key_exists('PHP_AUTH_PW', $_SERVER) ) {
+            $email = $_SERVER['PHP_AUTH_USER'];
+            $pw = $_SERVER['PHP_AUTH_PW'];
+            $sessionManager = $this->pageView->sessionManager;
+            
+            $loginSuccess = $sessionManager->login($email, $pw);
+            if ($loginSuccess)
+                $userID = $sessionManager->userId;
+        }
+
         return $userId;
     }
 
