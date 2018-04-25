a9e502920b6f8419be838afc4ec2df9e5843544d
Paul Anderson <elpablogrande@gmail.com>
2018-04-25 12:36:43 -0700

    Ext html element (#73)
    * ExtHtmlResource
    
    * dumb bug
    
    * dum bug
    
    * unit test

---
+++ toonces_library/php/resource/ExtHtmlResource.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @author paulanderson
+ * 
+ * ExtHtmlResource.php
+ * Initial commit: 4/25/2018
+ * 
+ * Acquires HTML content from a file referenced by the table ext_html_page table.
+ * 
+*/
+
+require_once LIBPATH.'php/toonces.php';
+
+class ExtHtmlResource extends HTMLResource implements iResource {
+
+    function getResource() {
+        // Query the database for this page's content HTML file 
+        $conn = $this->pageViewReference->getSQLConn();
+
+        $sql = 'SELECT html_path FROM ext_html_page WHERE page_id = :pageId';
+        $stmt = $conn->prepare($sql);
+
+        $stmt->execute(['pageId' => $this->pageViewReference->pageId]);
+        $result = $stmt->fetchAll();
+        $htmlPath = $result[0][0];
+
+        // Set HTML to the file referenced.
+        try {
+            $this->html = file_get_contents($htmlPath);
+        } catch (Exception $e) {
+            echo('Failed to get static HTML content: ' . $e->getMessage());
+            throw $e;
+        }
+        return $this->html;
+    }
+}
