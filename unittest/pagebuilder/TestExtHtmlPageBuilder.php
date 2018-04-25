272d2bee30f04a76c05ca0340d5c67f5f1e77b86
Paul Anderson <elpablogrande@gmail.com>
2018-04-25 12:36:13 -0700

    unit test

---
+++ unittest/pagebuilder/TestExtHtmlPageBuilder.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @author paulanderson
+ * 
+ * TestExtHtmlPageBuilder.php
+ * Initial commit: Paul Anderson, 4/25/2018
+ * 
+ * Unit test the ExtHtmlPageBuilder class
+ * 
+ */
+
+use PHPUnit\Framework\TestCase;
+
+require_once __DIR__ . '/../../config.php';
+require_once __DIR__ . '/../../toonces_library/php/toonces.php';
+require_once __DIR__ . '../../SqlDependentTestCase.php';
+
+class TestExtHtmlPageBuilder extends SqlDependentTestCase{
+    
+    /**
+     * @runInSeparateProcess
+     */
+    function testCreateContentElement() {
+        // ARRANGE
+        $conn = $this->getConnection();
+        $htmlPageView = new HTMLPageView(1);
+        $htmlPageView->setSQLConn($conn);
+        $this->buildTestDatabase();
+        $ehpb = new ExtHTMLPageBuilder($htmlPageView);
+
+        // ACT
+        $ehpb->createContentElement();
+
+        // ASSERT
+        $this->assertInstanceOf(ExtHtmlResource::class, $ehpb->contentElement);
+        
+        // TEARDOWN
+        $this->destroyTestDatabase();
+    }
+}
