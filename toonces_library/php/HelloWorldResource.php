9101438a4c9a439fc3b01b64bd29532419e47c9c
Paul <elpablogrande@gmail.com>
2018-10-04 18:45:38 -0700

    includes

---
+++ toonces_library/php/HelloWorldResource.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: paulanderson
+ * Date: 10/4/18
+ * Time: 6:43 PM
+ */
+
+class HelloWorldResource extends Resource
+{
+
+    public function __construct()
+    {
+        $this->getResponder = new HelloWorldResponder();
+    }
+
+}
--- toonces_library/php/toonces.php
+++ toonces_library/php/toonces.php
@@ -2,14 +2,26 @@
 
 //include_once LIBPATH . 'php/enum/HttpMethod.php';
 //include_once LIBPATH . 'php/enum/HttpResponseCode.php';
+
 include_once LIBPATH . 'php/Request.php';
 include_once LIBPATH . 'php/utility/static/SearchPathString.php';
 
 include_once LIBPATH . 'php/resourcefactory/interface/iResourceFactory.php';
-include_once LIBPATH . 'php/resourcefactory/TooncesResourceFactory.php';
-include_once LIBPATH . 'php/resource/abstract/Resource.php';
+// include_once LIBPATH . 'php/resourcefactory/TooncesResourceFactory.php';
+include_once LIBPATH . 'php/XmlResourceFactory.php';
 
+include_once LIBPATH . 'php/Response.php';
+include_once LIBPATH . 'php/Responder.php';
+
+include_once LIBPATH . 'php/DefaultResponse.php';
+include_once LIBPATH . 'php/DefaultResponder.php';
+include_once LIBPATH . 'php/HelloWorldResponder.php';
+
+include_once LIBPATH . 'php/iAuthenticator.php';
+include_once LIBPATH . 'php/DefaultAuthenticator.php';
 include_once LIBPATH . 'php/resource/abstract/Resource.php';
+include_once LIBPATH . 'php/HelloWorldResource.php';
+
 
 include_once LIBPATH . 'php/utility/static/UniversalConnect.php';
 
